af45bfcf26d8b8bccc9ff46499280b48b08083f2
yilei-zhang <yileizh@umich.edu>
2026-03-05 15:42:06 -0500

    change initial value notations

--- Shark/mcmc.jl
+++ Shark/mcmc.jl
@@ -21,12 +21,12 @@ n = length(samples)
 burnin = 50000
 iteration = 100000
 thin = 20
-w = [0.4, 0.4, 0.2]
-c = [-4.0, -3.2, -2.5]
-r = [0.4, 0.2, 0.4]
+w0 = [0.4, 0.4, 0.2]
+c0 = [-4.0, -3.2, -2.5]
+r0 = [0.4, 0.2, 0.4]
 
 @time w_all, c_all, r_all, g_result, β_all, 
-ϕ_all, σ_all, ind_all = MDP(samples; inits=(w, c, r), λ=0.8, ν=1, γ=2.0, vs=5., σs=0.072, wub=0.63,
+ϕ_all, σ_all, ind_all = MDP(samples; inits=(w0, c0, r0), λ=0.8, ν=1, γ=2.0, vs=5., σs=0.072, wub=0.63,
     burnin=burnin, iteration=iteration, thin=thin, multithreads=multithreads, seed=1465)
 
 results = (w_all, c_all, r_all, g_result, β_all, ϕ_all, σ_all, ind_all)
